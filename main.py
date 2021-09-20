c438275c8c084ec3a762dc368ca761505ac87298
MustafaTheCoder <73116655+MustafaTheCoder@users.noreply.github.com>
2021-09-20 10:26:57 +0500

    lol

---
+++ main.py
@@ -0,0 +1,48 @@
+import pyttsx3 
+import datetime
+import wikipedia 
+import os
+import smtplib
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wikipedia(arg):
+    speak('Searching Wikipedia...')
+    query = query.replace("wikipedia", "")
+    results = wikipedia.summary(query, sentences=2)
+    speak("According to Wikipedia")
+    print(results)
+    speak(results)
+
+while True:
+    user_inp = input("> ")
+
+    if user_inp == "open code".lower():
+        speak("Opening Code...")
+        code_path = "C://Users//MUHAMMAD DANISH//AppData//Local//Programs//Microsoft VS Code//Code.exe"
+        os.startfile(code_path)
+    elif user_inp == "open google".lower():
+        speak("Opening Google...")
+        google_path = "C://Program Files (x86)//Google//Chrome//Application//chrome.exe"
+        os.startfile(google_path)
+
+    elif user_inp == "the time":
+        strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+        speak(f"Sir, the time is {strTime}")
+
+    search = ""
+    elif user_inp == f"wikipedia {search}":
+        wikipedia(search)
+        
+    else:
+        break    
+
+
+
